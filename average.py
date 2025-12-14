35aed874a84e59f0490be1599cd83f0969d2c454
pitikpae-cyber <pitikpae@gmail.com>
2025-12-14 22:01:31 +0300

    Update average.py

--- average.py
+++ average.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) != 4: 
-print("Использование:python average.py <число1> <число2> <число3>")
+        print("Использование:python average.py <число1> <число2> <число3>")
 sys.exit(1)
 try:
         num1 = float(sys.argv[1])
@@ -11,4 +11,5 @@ try:
         print(f"Среднее: {average}")
 except ValueError: print("Ошибка: все аргументы должны быть числами!")
 sys.exit(1)
+
 
